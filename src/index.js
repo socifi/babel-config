f0e025b8338173e622b65e42437d18d5285da0df
Michal Ozogán <michal@ozogan.eu>
2019-10-21 09:48:29 +0200

    Changed: Export a single object instead of method BREAKING

--- src/index.js
+++ src/index.js
@@ -4,80 +4,89 @@
  * @param {false|string} modules - Compile modules, "amd" | "umd" | "systemjs" | "commonjs" | "cjs" | false
  * @returns {Object} Babel config
  */
-module.exports = (modules = 'commonjs') => {
-    return {
-        presets: [
-            '@babel/preset-react',
-            '@babel/preset-typescript',
-            [
-                '@babel/preset-env',
-                {
-                    modules,
-                    loose: true,
-                    useBuiltIns: 'usage',
-                    corejs: 3,
-                },
-            ],
+module.exports = {
+    presets: [
+        '@babel/preset-react',
+        '@babel/preset-typescript',
+        [
+            '@babel/preset-env',
+            {
+                modules: false,
+                loose: true,
+                useBuiltIns: 'usage',
+                corejs: 3,
+            },
         ],
-        plugins: [
-            '@babel/plugin-transform-runtime',
-            '@babel/plugin-proposal-object-rest-spread',
-            '@babel/plugin-proposal-class-properties',
-            'babel-plugin-transform-react-class-to-function',
-            [
-                '@babel/plugin-proposal-decorators',
-                {
-                    decoratorsBeforeExport: true,
-                },
-            ],
+    ],
+    plugins: [
+        '@babel/plugin-transform-runtime',
+        '@babel/plugin-proposal-object-rest-spread',
+        '@babel/plugin-proposal-class-properties',
+        'babel-plugin-transform-react-class-to-function',
+        [
+            '@babel/plugin-proposal-decorators',
+            {
+                decoratorsBeforeExport: true,
+            },
         ],
-        env: {
-            test: {
-                plugins: [
-                    'babel-plugin-transform-dynamic-import',
+    ],
+    env: {
+        test: {
+            presets: [
+                [
+                    '@babel/preset-env',
+                    {
+                        modules: 'commonjs',
+                        loose: true,
+                        useBuiltIns: 'usage',
+                        corejs: 3,
+                    },
                 ],
-            },
-            development: {
-                plugins: [
-                    'react-hot-loader/babel',
-                    'babel-plugin-typescript-to-proptypes',
-                    '@babel/plugin-syntax-dynamic-import',
-                ],
-            },
-            production: {
-                presets: [
-                    [
-                        'minify',
-                        {
-                            evaluate: false,
-                            simplify: false,
-                            // removeUndefined: false,
-                            // deadcode: false,
-                            // booleans: false,
-                            // builtIns: false,
-                            // consecutiveAdds: false,
-                            // flipComparisons: false,
-                            // guards: false,
-                            // infinity: false,
-                            // mangle: false,
-                            // memberExpressions: false,
-                            // numericLiterals: false,
-                            // propertyLiterals: false,
-                            // regexpConstructors: false,
-                            // replace: false,
-                            // simplifyComparisons: false,
-                            // typeConstructors: false,
-                            // undefinedToVoid: false,
-                            mergeVars: false,
-                            // removeConsole: false,
-                            // removeDebugger: false,
-                        },
-                    ],
-                ],
-                plugins: [
-                    '@babel/plugin-syntax-dynamic-import',
-                ],
-            },
+            ],
+            plugins: [
+                'babel-plugin-transform-dynamic-import',
+            ],
         },
-    };
+        development: {
+            plugins: [
+                'react-hot-loader/babel',
+                'babel-plugin-typescript-to-proptypes',
+                '@babel/plugin-syntax-dynamic-import',
+            ],
+        },
+        production: {
+            presets: [
+                [
+                    'minify',
+                    {
+                        evaluate: false,
+                        simplify: false,
+                        // removeUndefined: false,
+                        // deadcode: false,
+                        // booleans: false,
+                        // builtIns: false,
+                        // consecutiveAdds: false,
+                        // flipComparisons: false,
+                        // guards: false,
+                        // infinity: false,
+                        // mangle: false,
+                        // memberExpressions: false,
+                        // numericLiterals: false,
+                        // propertyLiterals: false,
+                        // regexpConstructors: false,
+                        // replace: false,
+                        // simplifyComparisons: false,
+                        // typeConstructors: false,
+                        // undefinedToVoid: false,
+                        mergeVars: false,
+                        // removeConsole: false,
+                        // removeDebugger: false,
+                    },
+                ],
+            ],
+            plugins: [
+                '@babel/plugin-syntax-dynamic-import',
+            ],
+        },
+    },
 };
